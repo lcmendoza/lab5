e2009036e066cf6e5fdf29a8b71cbc093fb5fcdb
Lereanne Mendoza <lcmendoz@ucsd.edu>
2019-02-07 10:05:59 -0800

    Updated and finished lab5

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
-// Example route
-// var user = require('./routes/user');
+var add = require('./routes/add');
 
 var app = express();
 
@@ -36,6 +35,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/add', add.addFriend);
 // Example route
 // app.get('/users', user.list);
 
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,19 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
+exports.addFriend = function(req, res) {    
 	// Your code goes here
- }
+	res.render('index', data);
+
+	var name = req.query.name;
+	var description = req.query.description;
+
+	var newFriend = {
+		"name" : "New Friend",
+		"description" : "Friend Description",
+		"imageURL" : "http://lorempixel.com/400/400/people"
+	};
+	
+	console.log(newFriend);
+	data.friends.push(newFriend);
+
+ } 
